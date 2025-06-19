5268dea740fa6ec7214c1b33d3a2cca78e3bfeed
suyashmhetre <mhetresuyash676@gmail.com>
2025-06-19 17:47:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let input = document.getElementsByClassName("brutalist-input smooth-type")[0];
+let heading = document.getElementsByClassName("number")[0];
+let fact = document.getElementsByClassName("fact")[0];
+let search = document.getElementsByClassName("button")[0];
+
+
+
+
+async function fetchapi() {
+  try {
+    let number = input.value;
+    const response = await fetch(`http://numbersapi.com/${number}/trivia`);
+    const data = await response.text();
+    fact.textContent=data;
+    heading.textContent= number;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+
